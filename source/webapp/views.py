35c683da617ba3e43431bf32051134d82cf223c2
RagdIR <ragdirstrogov@gmail.com>
2020-08-06 16:07:17 +0600

    Исправлены страницы, настройки

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
+from django.views.generic import View, TemplateView
+from webapp.models import Task
+from webapp.forms import TaskForm
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    def get(self, request):
+        is_admin = request.GET.get('is_admin', None)
+        data = Task.objects.all()
+        return render(request, 'index.html', context={
+            'tasks': data
+        })
+
+
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        task = get_object_or_404(Task, pk=pk)
+        context['task'] = task
+        return context
+
+
+class TaskCreateView(View):
+    def get(self, request):
+        return render(request, 'task_create.html', context={
+            'form': TaskForm()
+        })
+
+    def post(self, request):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            data = {}
+            for key, value in form.cleaned_data.items():
+                if value is not None:
+                    data[key] = value
+            task = Task.objects.create(**data)
+            return redirect('task_view', pk=task.pk)
+        else:
+            return render(request, 'task_create.html', context={
+                'form': form
+            })
+
+
+class TaskUpdateView(TemplateView):
+    template_name = 'task_update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pk = self.kwargs.get('pk')
+        task = get_object_or_404(Task, pk=pk)
+
+        initial = {}
+        for key in 'summary', 'description', 'status', 'type':
+            initial[key] = getattr(task, key)
+        form = TaskForm(initial=initial)
+
+        context['task'] = task
+        context['form'] = form
+
+        return context
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            for key, value in form.cleaned_data.items():
+                if value is not None:
+                    setattr(task, key, value)
+            task.save()
+            return redirect('task_view', pk=task.pk)
+        else:
+            return self.render_to_response({
+                'task': task,
+                'form': form
+            })
+
+
+class TaskDeleteView(TemplateView):
+    template_name = 'task_update.html'
+
+    def task_delete_view(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        if request.method == 'GET':
+            return render(request, 'task_delete.html', context={'task': task})
+        elif request.method == 'POST':
+            task.delete()
+            return redirect('index')
